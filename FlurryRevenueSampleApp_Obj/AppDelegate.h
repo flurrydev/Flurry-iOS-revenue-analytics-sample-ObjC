4ab086066cb259e479a6643dfc107261c129f5cb
Yilun Xu <yilun.xu@oath.com>
2018-10-03 09:52:59 -0700

    Initial Commit

---
+++ FlurryRevenueSampleApp_Obj/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  FlurryRevenueSampleApp_Obj
+//
+//  Created by Yilun Xu on 10/3/18.
+//  Copyright © 2018 Flurry. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ FlurryRevenueSampleApp_Obj/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  FlurryRevenueSampleApp_Obj
+//
+//  Created by Yilun Xu on 10/3/18.
+//  Copyright © 2018 Flurry. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
