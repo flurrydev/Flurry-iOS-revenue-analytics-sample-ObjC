0eddc94cf5fd2231455c687bcd7d8b06607bc861
Yilun Xu <yilun.xu@oath.com>
2018-10-04 13:26:32 -0700

    add ui

--- FlurryRevenueSampleApp_Obj/RevenueTableViewController.m
+++ FlurryRevenueSampleApp_Obj/RevenueTableViewController.m
@@ -30,13 +30,16 @@
     NSArray *productIdentifiers = [NSArray arrayWithContentsOfURL:url];
     self.products = [NSSet setWithArray:productIdentifiers];
     
-    //start request
+    //init request
     if (![SKPaymentQueue canMakePayments]) {
         return;
     }
     SKProductsRequest *request = [[SKProductsRequest alloc] initWithProductIdentifiers:self.products];
     request.delegate = self;
     [request start];
+    
+    // add self as an observer, be notified if one or more transactions are being updated
+    [[SKPaymentQueue defaultQueue] addTransactionObserver:self];
     
     // record toggle position
     NSUserDefaults *defaults =[NSUserDefaults standardUserDefaults];
@@ -49,14 +52,18 @@
     }
 }
 
-- (void)didReceiveMemoryWarning {
-    [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
+// purchase the product
+-(void)purchase:(SKProduct *)product {
+    if ([SKPaymentQueue canMakePayments]) {
+        // add this payment to the payment queue
+        SKPayment *payment = [SKPayment paymentWithProduct:product];
+        [[SKPaymentQueue defaultQueue] addPayment:payment];
+    }
 }
 
 #pragma mark - StoreKit delegate + observer
 
-// load all valid requests from app stroe into tableview
+// Accepts the reponse from app store that contains the request products information
 - (void)productsRequest:(SKProductsRequest *)request didReceiveResponse:(SKProductsResponse *)response{
     self.verifiedProducts = [NSMutableArray array];
     // invalid products
@@ -68,56 +75,6 @@
         [self.verifiedProducts addObject:product];
     }
     [self.tableView reloadData];
-}
-
-// one or more transactions has been updated
-- (void)paymentQueue:(SKPaymentQueue *)queue updatedTransactions:(NSArray<SKPaymentTransaction *> *)transactions {
-    for (SKPaymentTransaction *transaction in transactions) {
-        switch (transaction.transactionState) {
-            case SKPaymentTransactionStatePurchased:
-                if (!autoLogSwitch.isOn) {
-                    [Flurry logPaymentTransaction:transaction statusCallback:^(FlurryTransactionRecordStatus status) {
-                        [self displayAlertWithTitle:@"Success" message:[self stringForTransactionRecordStatus:status]];
-                    }];
-                } else {
-                    [self displayAlertWithTitle:@"Success" message:@"Apyment went througth successfully"];
-                }
-                [[SKPaymentQueue defaultQueue] finishTransaction:transaction];
-                break;
-            case SKPaymentTransactionStateFailed:
-                if (!autoLogSwitch.isOn) {
-                    [Flurry logPaymentTransaction:transaction statusCallback:^(FlurryTransactionRecordStatus status) {
-                        [self displayAlertWithTitle:@"Failed" message:[self stringForTransactionRecordStatus:status]];
-                    }];
-                } else {
-                    [self displayAlertWithTitle:@"Failed" message:@"Payment did not go througth"];
-                }
-                [[SKPaymentQueue defaultQueue] finishTransaction:transaction];
-                break;
-            case SKPaymentTransactionStateDeferred:
-                [self displayAlertWithTitle:@"Purchasing" message:nil];
-                break;
-            case SKPaymentTransactionStateRestored:
-                break;
-            case SKPaymentTransactionStatePurchasing:
-                break;
-        }
-    }
-    
-}
-
-// alert display
--(void)displayAlertWithTitle:(NSString *)title message:(NSString *)message {
-
-    // set alert controller
-    UIAlertController *controller = [UIAlertController alertControllerWithTitle:title message:message preferredStyle:UIAlertControllerStyleActionSheet];
-    
-    // present the alert
-    [self presentViewController:controller animated:YES completion:^{
-        dispatch_after(dispatch_time(DISPATCH_TIME_NOW, (int64_t)(1 * NSEC_PER_SEC)), dispatch_get_main_queue(), ^{
-                [controller dismissViewControllerAnimated:YES completion:nil];
-            });
-    }];
 }
 
 - (NSString *)stringForTransactionRecordStatus:(FlurryTransactionRecordStatus)status {
@@ -132,6 +89,58 @@
             return @"Recording Disabled";
     }
 }
+
+// Tell the observer when one or more transactions are being updated
+- (void)paymentQueue:(SKPaymentQueue *)queue updatedTransactions:(NSArray<SKPaymentTransaction *> *)transactions {
+    for (SKPaymentTransaction *transaction in transactions) {
+        switch (transaction.transactionState) {
+            case SKPaymentTransactionStatePurchased:
+                if (!autoLogSwitch.isOn) {
+                    [Flurry logPaymentTransaction:transaction statusCallback:^(FlurryTransactionRecordStatus status) {
+                        [self displayAlertWithTitle:@"Success" message:[self stringForTransactionRecordStatus:status]];
+                    }];
+                } else {
+                    [self displayAlertWithTitle:@"Success" message:@"Payment went througth successfully"];
+                }
+                [[SKPaymentQueue defaultQueue] finishTransaction:transaction];
+                break;
+            case SKPaymentTransactionStateFailed:
+                NSLog(@"%@", transaction.error.debugDescription);
+                if (!autoLogSwitch.isOn) {
+                    [Flurry logPaymentTransaction:transaction statusCallback:^(FlurryTransactionRecordStatus status) {
+                        [self displayAlertWithTitle:@"Failed" message:[self stringForTransactionRecordStatus:status]];
+                    }];
+                } else {
+                    [self displayAlertWithTitle:@"Failed" message:@"Payment did not go througth"];
+                }
+                [[SKPaymentQueue defaultQueue] finishTransaction:transaction];
+                break;
+            case SKPaymentTransactionStateDeferred:
+                break;
+            case SKPaymentTransactionStateRestored:
+                break;
+            case SKPaymentTransactionStatePurchasing:
+                break;
+        }
+    }
+    
+}
+
+#pragma mark - alert
+
+-(void)displayAlertWithTitle:(NSString *)title message:(NSString *)message {
+
+    // set alert controller
+    UIAlertController *controller = [UIAlertController alertControllerWithTitle:title message:message preferredStyle:UIAlertControllerStyleActionSheet];
+    
+    // present the alert
+    [self presentViewController:controller animated:YES completion:^{
+        dispatch_after(dispatch_time(DISPATCH_TIME_NOW, (int64_t)(1 * NSEC_PER_SEC)), dispatch_get_main_queue(), ^{
+                [controller dismissViewControllerAnimated:YES completion:nil];
+            });
+    }];
+}
+
 
 #pragma mark - Table view data source
 
@@ -150,11 +159,6 @@
 
 - (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath {
     UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:@"cell" forIndexPath:indexPath];
-    
-    return cell;
-}
-
-- (void)tableView:(UITableView *)tableView willDisplayCell:(UITableViewCell *)cell forRowAtIndexPath:(NSIndexPath *)indexPath{
     SKProduct *product = self.verifiedProducts[indexPath.row];
     
     cell.textLabel.text = product.localizedTitle;
@@ -174,16 +178,14 @@
     } else {
         cell.detailTextLabel.text = [NSString stringWithFormat:@"Price :  %@", product.price];
     }
-    
+    return cell;
 }
 
 - (void)tableView:(UITableView *)tableView didSelectRowAtIndexPath:(NSIndexPath *)indexPath{
     [tableView deselectRowAtIndexPath:indexPath animated:YES];
     NSLog(@"product id is : %@", self.verifiedProducts[indexPath.row].productIdentifier);
     if ([SKPaymentQueue canMakePayments]) {
-        SKPayment *payment = [SKPayment paymentWithProduct:self.verifiedProducts[indexPath.row]];
-        [[SKPaymentQueue defaultQueue] addTransactionObserver:self];
-        [[SKPaymentQueue defaultQueue] addPayment:payment];
+        [self purchase:self.verifiedProducts[indexPath.row]];
     } else {
         UIAlertController *controller = [UIAlertController alertControllerWithTitle:@"Error" message:@"Purchases are disabled in your device" preferredStyle:UIAlertControllerStyleAlert];
         UIAlertAction *okAction = [UIAlertAction actionWithTitle:@"OK" style:UIAlertActionStyleCancel handler:nil];
@@ -191,6 +193,8 @@
         [self presentViewController:controller animated:YES completion:nil];
     }
 }
+
+#pragma mark - update toggle
 
 - (IBAction)updateAutoLogSwitch:(UISwitch *)sender {
     [Flurry setIAPReportingEnabled:sender.isOn];
